1e34a12c9e200f3553b33dbfec8295477714dd2c
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-10-19 23:47:22 +0800

    Chore: Add env variable types

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 import authRouter from "./routes/authRouter";
 import eventRouter from "./routes/eventRouter";
 import accountRouter from "./routes/accountRouter";
@@ -10,6 +13,6 @@ app.use("/auth", authRouter);
 app.use("/event", eventRouter);
 app.use("/account", accountRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
