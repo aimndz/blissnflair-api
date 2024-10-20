a50a9f29a81e4f23b9f62c1fd20718c4f78f9296
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-10-20 21:20:27 +0800

    Feat: Add createAccount functionality on admin

--- controllers/accountController.ts
+++ controllers/accountController.ts
@@ -1,8 +1,9 @@
 import asyncHandler from "express-async-handler";
 import { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import bcrypt from "bcryptjs";
 
 import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
 
 const accountController = {
@@ -46,9 +47,98 @@ const accountController = {
     res.status(200).json(account);
   }),
 
-  createAccount: asyncHandler(async (req: Request, res: Response) => {
-    // TODO: Implement create account
-  }),
+  createAccount: [
+    body("firstName")
+      .trim()
+      .notEmpty()
+      .withMessage("First name is required")
+      .isLength({ max: 35 })
+      .withMessage("First name must be less than 35 characters"),
+
+    body("lastName")
+      .trim()
+      .notEmpty()
+      .withMessage("Last name is required")
+      .isLength({ max: 35 })
+      .withMessage("Last name must be less than 35 characters"),
+
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Invalid email format")
+      .custom(async (value) => {
+        const existingUser = await prisma.user.findUnique({
+          where: { email: value },
+        });
+        if (existingUser) {
+          throw new Error("Email already exists");
+        }
+        return true;
+      }),
+
+    body("phoneNumber").optional(),
+
+    body("role").optional().isIn(["ADMIN", "USER"]),
+
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters")
+      .matches(/\d/)
+      .withMessage("Password must contain a number")
+      .matches(/[a-zA-Z]/)
+      .withMessage("Password must contain a letter")
+      .matches(/[!@#$%^&*(),.?":{}|<>]/)
+      .withMessage("Password must contain a special character"),
+
+    body("confirmPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("Confirm password is required")
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords do not match");
+        }
+        return true;
+      }),
+
+    asyncHandler(async (req: Request, res: Response) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+      }
+
+      const {
+        firstName,
+        lastName,
+        email,
+        role = "USER",
+        phoneNumber,
+        password,
+      } = req.body;
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      await prisma.user.create({
+        data: {
+          firstName,
+          lastName,
+          email,
+          role: role,
+          phoneNumber,
+          password: hashedPassword,
+        },
+      });
+
+      res.status(201).json({ msg: "User created successfully" });
+    }),
+  ],
 
   updateAccount: asyncHandler(async (req: Request, res: Response) => {
     // TODO: Implement update account
