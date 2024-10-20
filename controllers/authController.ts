894a26e490986894cda5a186f27040143700f0b6
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-10-20 08:47:08 +0800

    Feat: Add sign up feature

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
@@ -42,9 +43,76 @@ const authController = {
     // TODO: Implement admin login
   }),
 
-  signUp: asyncHandler(async (req: Request, res: Response) => {
-    // TODO: Implement sign up
-  }),
+  signUp: [
+    body("firstName")
+      .trim()
+      .notEmpty()
+      .withMessage("First name is required")
+      .isLength({ max: 35 })
+      .withMessage("First name must be less than 35 characters"),
+
+    body("lastName")
+      .trim()
+      .notEmpty()
+      .withMessage("Last name is required")
+      .isLength({ max: 35 })
+      .withMessage("Last name must be less than 35 characters"),
+
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Invalid email format"),
+
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required")
+      .isLength({ min: 8 })
+      .withMessage("Password must be at least 8 characters")
+      .matches(/\d/)
+      .withMessage("Password must contain a number")
+      .matches(/[a-zA-Z]/)
+      .withMessage("Password must contain a letter")
+      .matches(/[!@#$%^&*(),.?":{}|<>]/)
+      .withMessage("Password must contain a special character"),
+
+    body("confirmPassword")
+      .trim()
+      .notEmpty()
+      .withMessage("Confirm password is required")
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords do not match");
+        }
+        return true;
+      }),
+
+    asyncHandler(async (req: Request, res: Response) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+      }
+
+      const { firstName, lastName, email, password } = req.body;
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      await prisma.user.create({
+        data: {
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      res.status(201).json({ msg: "User created successfully" });
+    }),
+  ],
 
   logOut: asyncHandler(async (req: Request, res: Response) => {
     // TODO: Implement log out
